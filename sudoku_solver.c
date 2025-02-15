505ed5755bd9de812edec41fc4769a1c22431924
Xzander014 <xzander014@gmail.com>
2025-02-15 20:20:08 +0530

    Updated Code

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-const int N = 9;
+#define SIZE 9 // Define the size of the Sudoku grid
 
-int check(int row, int column, int num, int grid[N][N]);
-void prnt(int grid[N][N]);
-int solver(int grid[N][N]);
-int isepty(int grid[N][N], int i, int j);
+// Function prototypes
+bool isValidPlacement(int row, int col, int num, int grid[SIZE][SIZE]);
+void printGrid(int grid[SIZE][SIZE]);
+bool solveSudoku(int grid[SIZE][SIZE]);
+bool findEmptyCell(int grid[SIZE][SIZE], int *row, int *col);
 
-int main() {
-    int grid[9][9] = {
+int main()
+{
+    // Sample Sudoku puzzle (0 represents an empty cell)
+    int grid[SIZE][SIZE] = {
         {5, 3, 0, 0, 7, 0, 0, 0, 0},
         {6, 0, 0, 1, 9, 5, 0, 0, 0},
         {0, 9, 8, 0, 0, 0, 0, 6, 0},
@@ -18,92 +21,110 @@ int main() {
         {7, 0, 0, 0, 2, 0, 0, 0, 6},
         {0, 6, 0, 0, 0, 0, 2, 8, 0},
         {0, 0, 0, 4, 1, 9, 0, 0, 5},
-        {0, 0, 0, 0, 8, 0, 0, 7, 9}
-    };
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}};
 
-    if (solver(grid)) {
-        printf("Result\n");
-        prnt(grid);
-    } else {
-        printf("No solution Exists\n");
+    // Solve the puzzle and print the result
+    if (solveSudoku(grid))
+    {
+        printf("Solved Sudoku:\n");
+        printGrid(grid);
+    }
+    else
+    {
+        printf("No solution exists.\n");
     }
 
     return 0;
 }
 
-int check(int row, int column, int num, int grid[N][N]) {
-    int startrow, startclm;
-    for (int i = 0; i < N; i++) {
-        if (grid[row][i] == num || grid[i][column] == num) {
-            return 0;
+// Function to check if placing 'num' at grid[row][col] is valid
+bool isValidPlacement(int row, int col, int num, int grid[SIZE][SIZE])
+{
+    int boxStartRow = row - row % 3;
+    int boxStartCol = col - col % 3;
+
+    // Check the row and column for duplicates
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (grid[row][i] == num || grid[i][col] == num)
+        {
+            return false;
         }
     }
-    startrow = row - row % 3;
-    startclm = column - column % 3;
-    for (int i = startrow; i < startrow + 3; i++) {
-        for (int j = startclm; j < startclm + 3; j++) {
-            if (grid[i][j] == num) {
-                return 0;
+
+    // Check the 3x3 subgrid for duplicates
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (grid[boxStartRow + i][boxStartCol + j] == num)
+            {
+                return false;
             }
         }
     }
-    return 1;
+    return true;
 }
 
-void prnt(int grid[N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%2d", grid[i][j]);
+// Function to print the Sudoku grid
+void printGrid(int grid[SIZE][SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            printf("%2d ", grid[i][j]);
         }
         printf("\n");
     }
 }
 
-int isepty(int grid[N][N], int i, int j) {
-    if (grid[i][j] == 0) {
-        return 1;
-    }
-    return 0;
-}
-
-int solver(int grid[N][N]) {
-    int row = -1, col = -1;
-    bool empty = false;
-
-    // Find the first empty cell
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (isepty(grid, i, j)) {
-                row = i;
-                col = j;
-                empty = true;
-                break;
+// Function to find an empty cell in the Sudoku grid
+bool findEmptyCell(int grid[SIZE][SIZE], int *row, int *col)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (grid[i][j] == 0)
+            { // Check if the cell is empty
+                *row = i;
+                *col = j;
+                return true;
             }
         }
-        if (empty)
-            break;
+    }
+    return false; // No empty cell found
+}
+
+// Function to solve the Sudoku puzzle using backtracking
+bool solveSudoku(int grid[SIZE][SIZE])
+{
+    int row, col;
+
+    // Find the next empty cell
+    if (!findEmptyCell(grid, &row, &col))
+    {
+        return true; // No empty cells left, puzzle is solved
     }
 
-    // If no empty cell is found, the puzzle is solved
-    if (!empty) {
-        return 1;
-    }
-
-    // Try placing numbers from 1 to 9 in the current cell
-    for (int num = 1; num <= 9; num++) {
-        if (check(row, col, num, grid)) {
+    // Try placing numbers from 1 to 9 in the empty cell
+    for (int num = 1; num <= SIZE; num++)
+    {
+        if (isValidPlacement(row, col, num, grid))
+        {
             grid[row][col] = num; // Place the number
 
-            // Recursively solve the rest of the puzzle
-            if (solver(grid)) {
-                return 1; // If solved, return success
+            // Recursively try solving the rest of the puzzle
+            if (solveSudoku(grid))
+            {
+                return true;
             }
 
-            // Backtrack: Reset the cell if the number doesn't lead to a solution
+            // Backtrack if no solution is found
             grid[row][col] = 0;
         }
     }
 
-    // If no number can be placed in the current cell, backtrack
-    return 0;
+    return false; // No valid number can be placed, trigger backtracking
 }
